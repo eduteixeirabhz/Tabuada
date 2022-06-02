508cefebd77649620ecb4137780808def3bd4f19
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 12:56:11 -0300

    Add files via upload

---
+++ Desafio09.py
@@ -0,0 +1,15 @@
+print('Bem vindo à tabuada digital !')
+n = int(input('Por favor digite um número para multiplicar: '))
+n1 = n*1
+n2 = n*2
+n3 = n*3
+n4 = n*4
+n5 = n*5
+n6 = n*6
+n7 = n*7
+n8 = n*8
+n9 = n*9
+n10 = n*10
+print('Seu número multiplicado por 1 é: {} Seu número multiplicado por 2 é: {}'.format(n1, n2))
+
+
